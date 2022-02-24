e6174aa3ac1259884ec7074b0466a68facee53ca
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-02-24 14:41:18 +0000

    refactored code

--- assets/js/instructionsScene.js
+++ assets/js/instructionsScene.js
@@ -11,26 +11,27 @@ let instructionsScene = Phaser.Class({
     },
     
     create: function () {
-        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-
         backgroundImage = this.add.image(320, 256, 'instructions-background');
         backgroundImage.displayWidth = this.sys.canvas.width;
 
-        const title = "Instructions";
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
 
-        let titleText = this.add.text(screenCenterX, 120, title, {fontSize: "60px", fill: 'white'}).setOrigin(0.5);
-        titleText.setShadow(2, 2, "#333333", 2, false, true);
+        let title = this.add.text(screenCenterX, 120, "Instructions", {fontSize: "60px", fill: 'white'}).setOrigin(0.5);
+        title.setShadow(2, 2, "#333333", 2, false, true);
 
-        let instructions = [
+        let instructionsContent = [
             '- Place turrets to kill the enemies!\n',
             '- Click on the turrets to upgrade or sell\n',
             '- lorem\n'
         ];
 
-        let instructionsText = this.add.text(80, 250, instructions, {fontSize: 20});
-        instructionsText.setColor(textColor);
+        let instructions = this.add.text(80, 250, instructionsContent, {fontSize: 20});
+        instructions.setColor(textColor);
 
-        let returnButton = new Button(screenCenterX, 500, 'Return to Main Menu', this, () => { this.scene.start('mainMenuScene')}, 50, 10);
+        let returnButton = new Button(screenCenterX, 500, 'Return to Main Menu', this, () => {
+            this.scene.start('mainMenuScene')
+        }, 50, 10);
+        
         returnButton;
     }
 });
