bd17da6e05387defe682fe433deabb4a081207ae
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-02-24 00:18:25 +0000

    replaced click on text with buttons and added some colors

--- assets/js/mainMenuScene.js
+++ assets/js/mainMenuScene.js
@@ -6,28 +6,44 @@ let mainMenuScene = Phaser.Class({
         },
 
     preload: function() {
-        
+        this.load.image('menu-background', 'assets/media/main-menu-background.jpg');
     },
     
     create: function () {
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
 
-        let title = "Tower Defence";
-        let optionsFont = "25px Arial";
-        
-        let loadingText = this.add.text(screenCenterX, 100, title, {font: "50px Arial",fill: 'white'}).setOrigin(0.5);
-        loadingText;
+        backgroundImage = this.add.image(320,256,'menu-background');
+        backgroundImage.displayWidth = this.sys.canvas.width;
+        backgroundImage.displayHeight = 800;
 
-        let playText = this.add.text(screenCenterX, 250, "Play Game", {font: optionsFont}).setOrigin(0.5);
-        playText.setInteractive();
-        playText.on('pointerdown', () => { this.scene.start('gameScene'); this.scene.start("UIScene") });
-
-        let loginText = this.add.text(screenCenterX, 300, "Log In", {font: optionsFont}).setOrigin(0.5);
-        loginText;
+        const title = "Orcs & Cannons";
+        const textColor= '#FFE77AFF';
+        const brownColor= '#4A2619';
+        const goldColor = '#F39C12';
         
-        let instructionsText = this.add.text(screenCenterX, 350, "Instructions", {font: optionsFont}).setOrigin(0.5);
-        instructionsText.setInteractive();
-        instructionsText.on('pointerdown', () => {this.scene.start('instructionsScene')});
-    },
+        let titleText = this.add.text(screenCenterX, 120, title, {fontSize: "60px Calibri",fill: textColor}).setOrigin(0.5);
+        titleText;
+
+        class Button {
+            constructor(x, y, label, scene, callback, paddingWidth, paddingHeight) {
+                const button = scene.add.text(x, y, label)
+                    .setOrigin(0.5)
+                    .setPadding(paddingWidth, paddingHeight)
+                    .setStyle({ backgroundColor: brownColor, fontSize: '25px', })
+                    .setColor(textColor)
+                    .setInteractive({ useHandCursor: true })
+                    .on('pointerdown', () => callback())
+                    .on('pointerover', () => button.setStyle({ fill: brownColor, backgroundColor: goldColor}))
+                    .on('pointerout', () => button.setStyle({ fill: textColor, backgroundColor: brownColor}));
+            }
+        }
+
+        let buttonGame = new Button(screenCenterX, 300, 'Play Game', this, () => { this.scene.start('gameScene'); this.scene.start("UIScene")}, 50, 10);
+        let buttonLogIn = new Button(screenCenterX, 375, 'Log In', this, () => console.log('clicked on login'), 72, 10);
+        let buttonInstructions = new Button(screenCenterX, 450, 'Instructions', this, () => {this.scene.start('instructionsScene')}, 27, 10);
+
+        buttonGame, buttonLogIn, buttonInstructions;
+    }
+
 });
