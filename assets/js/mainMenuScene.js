ea660f46abdf4e8466d87bf95ee143921b0fd04c
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-02-24 12:42:43 +0000

    changed theme of instructions scene + added content

--- assets/js/mainMenuScene.js
+++ assets/js/mainMenuScene.js
@@ -15,16 +15,16 @@ let mainMenuScene = Phaser.Class({
 
         backgroundImage = this.add.image(320,256,'menu-background');
         backgroundImage.displayWidth = this.sys.canvas.width;
-        backgroundImage.displayHeight = 800;
+        // backgroundImage.displayHeight = 800;
 
-        const title = "Tower Defence Game";
+        const title = "Tower Game";
         // const textColor= '#FFE77AFF';
         const textColor = 'white';
         const buttonBackgroundColor = '#363636'
         const goldColor = '#F39C12';
         
-        let titleText = this.add.text(screenCenterX, 120, title, {fontSize: "60px Calibri",fill: textColor}).setOrigin(0.5);
-        titleText;
+        let titleText = this.add.text(screenCenterX, 120, title, {fontSize: "60px",fill: textColor}).setOrigin(0.5);
+        titleText.setShadow(2, 2, "#333333", 2, false, true);
 
         class Button {
             constructor(x, y, label, scene, callback, paddingWidth, paddingHeight) {
