34dec03cea54f18168e289d892ef726ff51d7496
JACK SMITH <c0037268@hallam.shu.ac.uk>
2022-03-28 14:04:48 +0100

    database login system

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
+require_once "process/config.php";
 
-require_once "config.php";
-
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,10 +21,46 @@ require_once "config.php";
         <main>
             <div id="game"></div>
 
-            <div id="input-box" style="display: none">
-                <input type="text" name="username" placeholder="Username"><br/>
-                <input type="text" name="password" placeholder="Password"><br/>
-                <button type="submit">Submit</button>
+            <div id="input-box-login" style="display: none">
+                <form action="<?php echo htmlspecialchars("process/login.php"); ?>" method="post">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                        <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                    </div>    
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                        <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                    </div>
+                        <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Login" >
+                    </div>
+                </form>
+            </div>
+
+            <div id="input-box-register" style="display: none">
+                <form action="<?php echo htmlspecialchars("process/register.php"); ?>" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                    <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                </div>    
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                    <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+                </div>                
+                </form>
             </div>
         </main>
 
@@ -32,6 +68,7 @@ require_once "config.php";
 
         <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
         <script src="assets/js/loginScene.js"></script>
+        <script src="assets/js/registerScene.js"></script>
         <script src="assets/js/mainMenuScene.js"></script>
         <script src="assets/js/gameObjects.js"></script>
         <script src="assets/js/gameScene.js"></script>
